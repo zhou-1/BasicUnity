81d667305ee71212dac6a467d37fe29c588e2720
Zhou <gentle_zhou_shen@outlook.com>
2020-04-15 23:46:20 -0700

    Add files via upload

---
+++ SimulatorTrumpAgainstCOVID19-TEXTGame/AdventureGame.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+
+    State state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;
+        textComponent.text = state.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();  // var can be replaced of State[]
+
+        for(int index = 0; index < nextStates.Length; index++)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + index)) //smart using + index
+            {
+                state = nextStates[index];
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Application.Quit();
+        }
+
+        textComponent.text = state.GetStateStory();
+    }
+}
---
+++ SimulatorTrumpAgainstCOVID19-TEXTGame/State.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    [TextArea(14,10)] [SerializeField] string storyText;  //14: height; 10: line of view can be scroll
+    [SerializeField] State[] nextStates;
+
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+
+    public State[] GetNextStates()
+    {
+        return nextStates;
+    }
+
+
+}
